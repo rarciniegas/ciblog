005e672d5e03e1f7908251b31658061c3160a0e8
Roberto Arciniegas <rarciniegas@hotmail.com>
2019-07-01 20:24:06 -0400

    ciBlog is a CodeIgniter Blog from the tutorial by Brad Traversy https://www.traversymedia.com/ This is the initial commit.

---
+++ application/views/templates/header.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+    <title>ciBlog</title>
+  </head>
+  <body>
+
+
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-3">
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="container">
