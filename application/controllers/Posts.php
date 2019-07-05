9fda2a6f75b782e12e223acdfaf4682e9bed5646
Roberto Arciniegas <rarciniegas@hotmail.com>
2019-07-05 10:43:47 -0400

    Add login and logout functionality and add the access control modified:   application/controllers/Categories.php modified:   application/controllers/Posts.php modified:   application/models/Post_model.php modified:   application/views/posts/view.php modified:   application/views/templates/header.php new file:   application/views/users/login.php new file:   assets/images/posts/OS_Ubuntu.png

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -23,6 +23,10 @@
 		}
 
 		public function create(){
+			// Check login
+			if(!$this->session->userdata('logged_in')){
+				redirect('users/login');
+			}
 			
 			$data['title'] = 'Create Post';
 			$data['categories'] = $this->post_model->get_categories();
@@ -62,6 +66,10 @@
 
 		public function delete($id){
 
+		// Check login
+			if(!$this->session->userdata('logged_in')){
+				redirect('users/login');
+			}
 			$this->post_model->delete_post($id);
 			// Set message
 			$this->session->set_flashdata('post_deleted', 'Your post has been deleted');
@@ -69,7 +77,17 @@
 		}
 
 		public function edit($slug){
+			// Check login
+			if(!$this->session->userdata('logged_in')){
+				redirect('users/login');
+			}
+
 			$data['post'] = $this->post_model->get_posts($slug);
+			// Check user
+			if($this->session->userdata('user_id') != $this->post_model->get_posts($slug)['user_id']){
+					redirect('posts');
+			}
+
 			$data['categories'] = $this->post_model->get_categories();
 			
 			if(empty($data['post'])){
@@ -82,6 +100,10 @@
 		}
 
 		public function update(){
+			// Check login
+			if(!$this->session->userdata('logged_in')){
+				redirect('users/login');
+			}
 			$this->post_model->update_post();
 			// Set message
 			$this->session->set_flashdata('post_updated', 'Your post has been updated');
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -27,16 +27,25 @@
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create Category</a>
-            </li>
-
+            <?php if(!$this->session->userdata('logged_in')) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
+                </li>
+            <?php endif;?>
+            <?php if($this->session->userdata('logged_in')) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create Category</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a>
+                </li>
+            <?php endif;?>
         </ul>
     </div>
   </nav>
